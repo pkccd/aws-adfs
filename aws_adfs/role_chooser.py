47347ecaf76a36b8eafd786aeaab51257940f0af
pkccd <patrick.lefler@kccd.edu>
2018-12-05 14:24:43 -0800

    Update role_chooser.py

--- aws_adfs/role_chooser.py
+++ aws_adfs/role_chooser.py
@@ -44,9 +44,9 @@ def choose_role_to_assume(config, principal_roles):
             click.echo('{}:'.format(account_name))
             for role_arn in roles.keys():
                 role_entry = roles[role_arn]
-                click.echo('    [ {} -> {} ]: {}'.format(role_entry['name'].ljust(30, ' ' if i % 2 == 0 else '.'), i, role_arn))
+                click.echo('  [ {} ]:  {} : {}'.format(i,(role_arn.split(":")[4]),role_entry['name']))
                 i += 1
-
+                
         selected_index = click.prompt(text='Selection', type=click.IntRange(0, len(role_collection)))
 
         chosen_principal_arn = role_collection[selected_index][0]
